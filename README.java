2b23a7f220606f0d89a13d1b9b516ed7fdc7b302
malathi-kpriet <malathikanakaraj14@gmail.com>
2016-07-09 16:23:12 +0530

    finish

---
+++ README.java
@@ -0,0 +1,15 @@
+# power
+class power
+{
+public static void  main(string args[])
+{
+int num=25;
+if(num==(num*num)
+{
+System.out.println("power"); 
+}
+else
+{System.out.println(" no power");
+}
+
+}}
